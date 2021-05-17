756b30b15075733bda995282c856503a93b33713
TalekC-137 <pawatl2002@gmail.com>
2021-05-17 15:43:00 +0200

    optymalizacja

--- app/src/main/java/com/scollon/markk/MainActivity.kt
+++ app/src/main/java/com/scollon/markk/MainActivity.kt
@@ -3,12 +3,9 @@ package com.scollon.markk
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.View.GONE
-import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Toast
-import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.Exception
 import java.util.*
@@ -20,6 +17,16 @@ class MainActivity : AppCompatActivity() {
     var points = 0; var kliknięcia = 1; var limit = 0;
     var doLosowani = 16;
     var bloczki1 = listOf(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+    private val guziki by lazy { arrayOf(
+        findViewById<Button>(R.id.button),  findViewById(R.id.button9),
+        findViewById(R.id.button2),  findViewById(R.id.button10),
+        findViewById(R.id.button3),  findViewById(R.id.button11),
+        findViewById(R.id.button4),  findViewById(R.id.button12),
+        findViewById(R.id.button5),  findViewById(R.id.button13),
+        findViewById(R.id.button6),  findViewById(R.id.button14),
+        findViewById(R.id.button7),  findViewById(R.id.button15),
+        findViewById(R.id.button8),  findViewById(R.id.button16),
+    )}
 
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,13 +42,15 @@ class MainActivity : AppCompatActivity() {
              //   losowanie()
                 losowanko(liczbaBloczkow)
                  kliknięcia++
-                kolorowanie((losowanko(liczbaBloczkow)))
+                test(losowanko(liczbaBloczkow))
+            //    kolorowanie((losowanko(liczbaBloczkow)))
                 liczbaBloczkow++
             }else {
                 Toast.makeText(this, "można losować tylko raz", Toast.LENGTH_LONG).show()
             }
         }
         btn_submit.setOnClickListener(){
+
 
 
 
@@ -83,54 +92,31 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    fun kolorowanie( bloczki: List<Int>) {
+    fun test( bloczki: List<Int>) {
 
 
-        if(bloczki.contains(1)){ button.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(2)){ button2.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(3)){ button3.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(4)){ button4.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(5)){ button5.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(6)){ button6.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(7)){ button7.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(8)){ button8.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(9)){ button9.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(10)){ button10.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(11)){ button11.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(12)){ button12.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(13)){ button13.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(14)){ button14.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(15)){ button15.setBackgroundResource(R.drawable.kafelek2) }
-        if(bloczki.contains(16)){ button16.setBackgroundResource(R.drawable.kafelek2) }
-
+        for(i in guziki.indices) {
+            if(bloczki.contains(i)) { guziki[i].setBackgroundResource(R.drawable.kafelek2)}
+        }
         Timer("waiting", false).schedule(2000) {
 
             zamienNaPodstawe()
         }
     }
+
+
 fun random(liczba: Int):Int{
     val rand1 = (0..liczba).random()
     return rand1
 }
 
-    fun zamienNaPodstawe(){
-        button.setBackgroundResource(R.drawable.kafelek)
-        button2.setBackgroundResource(R.drawable.kafelek)
-        button3.setBackgroundResource(R.drawable.kafelek)
-        button4.setBackgroundResource(R.drawable.kafelek)
-        button5.setBackgroundResource(R.drawable.kafelek)
-        button6.setBackgroundResource(R.drawable.kafelek)
-        button7.setBackgroundResource(R.drawable.kafelek)
-        button8.setBackgroundResource(R.drawable.kafelek)
-        button9.setBackgroundResource(R.drawable.kafelek)
-        button10.setBackgroundResource(R.drawable.kafelek)
-        button11.setBackgroundResource(R.drawable.kafelek)
-        button12.setBackgroundResource(R.drawable.kafelek)
-        button13.setBackgroundResource(R.drawable.kafelek)
-        button14.setBackgroundResource(R.drawable.kafelek)
-        button15.setBackgroundResource(R.drawable.kafelek)
-        button16.setBackgroundResource(R.drawable.kafelek)
 
+
+    fun zamienNaPodstawe(){
+
+            for(i in guziki.indices){
+                guziki[i].setBackgroundResource(R.drawable.kafelek)
+            }
     }
 
 // n equal to number of blocks to choose and equal to (n of rounds +2)
@@ -152,6 +138,9 @@ fun random(liczba: Int):Int{
     }
     return wylosowane
 }
+
+
+
 
 
 
